7be560cc9b5e4451979bedeadacffab6bf67b91c
Alec Chen <alecchendev@gmail.com>
2020-12-05 23:12:30 -0600

    Implement basic webgl routine to draw a square

--- www/index.js
+++ www/index.js
@@ -1,5 +1,6 @@
 import { Universe, Cell } from "runner-game";
 import { memory } from "runner-game/runner_game_bg";
+const { mat4, mat3, vec3 } = glMatrix;
 
 const CELL_SIZE = 5;
 const GRID_COLOR = "#CCCCCC";
@@ -10,11 +11,183 @@ const universe = Universe.new();
 const width = universe.width();
 const height = universe.height();
 
-const canvas = document.getElementById("runner-game-canvas");
-canvas.width = (CELL_SIZE + 1) * width + 1;
-canvas.height = (CELL_SIZE + 1) * height + 1;
+function initShaderProgram(gl, vsSource, fsSource) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
-const ctx = canvas.getContext('2d');
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    return null;
+  }
+
+  return shaderProgram;
+}
+
+function loadShader(gl, type, source) {
+  const shader = gl.createShader(type);
+
+  gl.shaderSource(shader, source);
+
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert('An error occurred compiling the shaders ' + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+function initBuffers(gl) {
+  const positionBuffer = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  const positions = [
+    4.0, 1.0,
+    -1.0, 1.0,
+    1.0, -1.0,
+    -1.0, -1.0,
+  ];
+
+  gl.bufferData(gl.ARRAY_BUFFER,
+                new Float32Array(positions),
+                gl.STATIC_DRAW);
+  return {
+    position: positionBuffer,
+  };
+}
+
+function drawScene(gl, programInfo, buffers) {
+  gl.clearColor(0.012, 0.647, 0.988, 1.0);
+  gl.clearDepth(1.0);
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LEQUAL);
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  const fieldOfView = 45 * Math.PI / 180;
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+  const zNear = 0.1;
+  const zFar = 100.0;
+
+  const projectionMatrix = mat4.create();
+  mat4.perspective(projectionMatrix,
+                  fieldOfView,
+                  aspect,
+                  zNear,
+                  zFar);
+
+  const modelViewMatrix = mat4.create();
+  mat4.translate(modelViewMatrix,
+                modelViewMatrix,
+                [-0.0, 0.0, -6.0]);
+  
+  {
+    const numComponents = 2;  // pull out 2 values per iteration - 2d..?
+    const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+    const normalize = false;  // don't normalize
+    const stride = 0;         // how many bytes to get from one set of values to the next
+                              // 0 = use type and numComponents above
+    const offset = 0;         // how many bytes inside the buffer to start from
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexPosition,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset);
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexPosition);
+  }
+
+  // Tell WebGL to use our program when drawing
+
+  gl.useProgram(programInfo.program);
+
+  // Set the shader uniforms
+
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.projectionMatrix,
+      false,
+      projectionMatrix);
+  gl.uniformMatrix4fv(
+      programInfo.uniformLocations.modelViewMatrix,
+      false,
+      modelViewMatrix);
+
+  {
+    const offset = 0;
+    const vertexCount = 4;
+    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
+  }
+}
+
+
+function main() {
+  const canvas = document.getElementById("runner-game-canvas");
+  canvas.width = 720;
+  canvas.height = 480;
+
+  const gl = canvas.getContext('webgl');
+
+  if (gl === null) {
+      alert("unable to initialize WebGL. Your browser or machine may not support it.");
+      return;
+  }
+
+  
+
+  // SHADERS
+
+  // Vertex shader
+  const vsSource = `
+  attribute vec4 aVertexPosition;
+
+  uniform mat4 uModelViewMatrix;
+  uniform mat4 uProjectionMatrix;
+
+  void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+  }
+
+  `;
+
+  // Fragment shader
+  const fsSource = `
+  void main() {
+    gl_FragColor = vec4(0.988, 0.012, 0.647, 1.0);
+  }
+  `;
+
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+  const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+    }
+  };
+
+  const buffers = initBuffers(gl);
+
+  drawScene(gl, programInfo, buffers);
+}
+
+window.onload = main;
+
+/*
 
 const getIndex = (row, column) => {
   return row * width + column;
@@ -73,3 +246,5 @@ const renderLoop = () => {
 drawGrid();
 drawCells();
 requestAnimationFrame(renderLoop);
+
+*/
