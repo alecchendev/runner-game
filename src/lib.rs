b10e01f64d160a21e044b762c95134b4c50a3535
Alec Chen <alecchendev@gmail.com>
2020-12-10 22:50:32 -0600

    Implement surfaces, triangles, and quadrilaterals

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-use std::fmt;
 extern crate js_sys;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -15,11 +14,15 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 extern "C" {
     // Use `js_namespace` here to bind `console.log(..)` instead of just
     // `log(..)`
+    #[wasm_bindgen(js_namespace = console, js_name = log)]
+    fn log_u32(a: u32);
+
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
 }
 
-struct Point {
+#[derive(Clone, Copy)]
+pub struct Point {
     x: f32,
     y: f32,
     z: f32,
@@ -32,6 +35,10 @@ impl Point {
             y,
             z,
         }
+    }
+
+    fn to_vec(&self) -> Vec<f32> {
+        vec![self.x, self.y, self.z]
     }
 }
 
@@ -154,7 +161,7 @@ impl Player {
     }
 
     pub fn position(&self) -> Vec<f32> {
-        vec![self.position.x, self.position.y, self.position.z]
+        self.position.to_vec()
     }
 
     pub fn theta(&self) -> f32 {
@@ -170,9 +177,65 @@ impl Player {
     }
 }
 
+#[derive(Clone)]
+pub enum Surface {
+    Tri(Tri),
+    Quad(Quad),
+}
+
+#[derive(Clone)]
+pub struct Tri {
+    vertices: [Point; 3],
+}
+
+impl Tri {
+    pub fn new(v1: Point, v2: Point, v3: Point) -> Self {
+        Self {
+            vertices: [v1, v2, v3],
+        }
+    }
+    pub fn to_vertices(&self) -> Vec<f32> {
+        let mut vertices = Vec::new();
+        vertices.append(&mut self.vertices[0].to_vec().clone());
+        vertices.append(&mut self.vertices[1].to_vec().clone());
+        vertices.append(&mut self.vertices[2].to_vec().clone());
+        vertices
+    }
+
+    pub fn to_indices(&self) -> Vec<u32> {
+        vec![0, 1, 2]
+    }
+}
+
+#[derive(Clone)]
+pub struct Quad {
+    tris: [Tri; 2],
+}
+
+impl Quad {
+    pub fn new(v1: Point, v2: Point, v3: Point, v4: Point) -> Self {
+        Self {
+            tris: [Tri::new(v1, v2, v3), Tri::new(v1, v3, v4)]
+        }
+    }
+    pub fn to_vertices(&self) -> Vec<f32> {
+        let mut vertices = Vec::new();
+        vertices.append(&mut self.tris[0].vertices[0].to_vec().clone());
+        vertices.append(&mut self.tris[0].vertices[1].to_vec().clone());
+        vertices.append(&mut self.tris[0].vertices[2].to_vec().clone());
+        vertices.append(&mut self.tris[1].vertices[2].to_vec().clone());
+        vertices
+    }
+
+    pub fn to_indices(&self) -> Vec<u32> {
+        vec![0, 1, 2, 0, 2, 3]
+    }
+}
+
 #[wasm_bindgen]
 #[derive(Clone)]
 pub struct Universe {
+    surfaces: Vec<Surface>,
     positions: Vec<f32>,
     colors: Vec<f32>,
     indices: Vec<u32>,
@@ -181,8 +244,74 @@ pub struct Universe {
 #[wasm_bindgen]
 impl Universe {
     pub fn new() -> Universe {
+        let surfaces = vec![
+            Surface::Quad(Quad::new(
+                Point::new(-1.0, -1.0,  1.0),
+                Point::new(1.0, -1.0,  1.0),
+                Point::new(1.0,  1.0,  1.0),
+                Point::new(-1.0,  1.0,  1.0),
+            )),
+            Surface::Quad(Quad::new(
+                Point::new(-1.0, -1.0,  -1.0),
+                Point::new(-1.0, 1.0,  -1.0),
+                Point::new(1.0,  1.0,  -1.0),
+                Point::new(1.0,  -1.0,  -1.0),
+            )),
+            Surface::Quad(Quad::new(
+                Point::new(-1.0, 1.0,  -1.0),
+                Point::new(-1.0, 1.0,  1.0),
+                Point::new(1.0,  1.0,  1.0),
+                Point::new(1.0,  1.0,  -1.0),
+            )),
+            Surface::Quad(Quad::new(
+                Point::new(-1.0, -1.0,  -1.0),
+                Point::new(1.0, -1.0,  -1.0),
+                Point::new(1.0, -1.0,  1.0),
+                Point::new(-1.0,  -1.0,  1.0),
+            )),
+            Surface::Quad(Quad::new(
+                Point::new(1.0, -1.0,  -1.0),
+                Point::new(1.0, 1.0,  -1.0),
+                Point::new(1.0,  1.0,  1.0),
+                Point::new(1.0,  -1.0,  1.0),
+            )),
+            Surface::Quad(Quad::new(
+                Point::new(1.0, 0.1, -1.0),
+                Point::new(1.0, -0.1, 1.0),
+                Point::new(3.0, -0.1, 1.0),
+                Point::new(3.0, 0.1, -1.0),
+            ))
+        ];
+
+        let mut positions = vec![];
+        let mut indices = vec![];
+        let mut next_index = 0;
+        for surface in &surfaces {
+            let mut new_vertices = match surface {
+                Surface::Quad(quad) => quad.to_vertices(),
+                Surface::Tri(tri) => tri.to_vertices(),
+            };
+            positions.append(&mut new_vertices);
+            let mut index_add = 0;
+            let mut new_indices = match surface {
+                Surface::Quad(quad) => {
+                    index_add = 4;
+                    quad.to_indices()
+                },
+                Surface::Tri(tri) => {
+                    index_add = 3;
+                    tri.to_indices()
+                },
+            };
+            for index in &mut new_indices {
+                *index += next_index;
+                log_u32(*index);
+            }
+            indices.append(&mut new_indices);
+            next_index += index_add;
+        }
         
-        let positions = vec![
+        /*let positions = vec![
             // Front face
             -1.0, -1.0,  1.0,
              1.0, -1.0,  1.0,
@@ -213,24 +342,27 @@ impl Universe {
              1.0,  1.0,  1.0,
              1.0, -1.0,  1.0,
             
-        ];
+        ];*/
         let colors = vec![
             1.0,  1.0,  1.0,  1.0,    // Front face: white
             1.0,  0.0,  0.0,  1.0,    // Back face: red
             0.0,  1.0,  0.0,  1.0,    // Top face: green
             0.0,  0.0,  1.0,  1.0,    // Bottom face: blue
             1.0,  1.0,  0.0,  1.0,    // Right face: yellow
+            1.0,  0.0,  1.0,  1.0,    // Right face: yellow
         ];
 
-        let indices = vec![
+        /*let indices = vec![
             0,  1,  2,      0,  2,  3,    // front
             4,  5,  6,      4,  6,  7,    // back
             8,  9,  10,     8,  10, 11,   // top
             12, 13, 14,     12, 14, 15,   // bottom
             16, 17, 18,     16, 18, 19,   // right
             //20, 21, 22,     20, 22, 23,   // left
-        ];
+        ];*/
+
         Universe {
+            surfaces,
             positions,
             colors,
             indices,
